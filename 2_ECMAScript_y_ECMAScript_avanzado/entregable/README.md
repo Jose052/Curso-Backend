ddbfc88bf2851f27aa968d47fbd1d193fb7d6531
Jose Valdes <jvaldes@macroaplicaciones.com>
2023-12-08 14:28:44 -0600

    Entregable ecmascrip y ecmascrip avanzado

---
+++ 2_ECMAScript_y_ECMAScript_avanzado/entregable/README.md
@@ -0,0 +1,5 @@
+Se crearon clases para dos administradores (productos y usuarios), los cuales contienen metodos para poder manipular la informacion dentro de los arrays de productos y usuarios, en este caso para poder crear usuarios con propiedades id, name, photo, email y productos con propiedades id, title, photo, price, stock.
+
+Ademas de que al momento de crear algun producto o usuario se autoincrementaran los id y tambien arrojara un error en caso de que no se incluyan los datos corresponidentes.
+
+Tambien se incluyen dos metodos para consultar todos los registro o solamente uno mediante su id, en caso de que su id no exista se arrojara un error
---
+++ 2_ECMAScript_y_ECMAScript_avanzado/entregable/UserManager.js
@@ -0,0 +1,61 @@
+class UserManager {
+  constructor() {
+    this.Users = [];
+  }
+
+  create(name, photo, email) {
+    if (!name || !photo || !email) {
+      console.log("todos los campos son obligatorios!");
+    }
+    const id = this.Users.length ? this.Users[this.Users.length - 1].id + 1 : 1;
+    const newUser = { id, name, photo, email };
+    this.Users.push(newUser);
+  }
+
+  read() {
+    return this.Users;
+  }
+
+  readOne(id) {
+    const userById = this.Users.filter((user) => user.id === id);
+    if (userById.length == 0) {
+      return `Producto con el id ${id} no encontrado`;
+    }
+    return userById;
+  }
+}
+
+
+const manager = new UserManager()
+
+const env =()=>{
+    console.log('se crean dos usuarios')
+    manager.create(
+        "usuario 1",
+        "sin foto",
+        "usuario1@emial.com"
+        )
+    manager.create(
+        "usuario 2",
+        "sin foto",
+        "usuario2@emial.com"
+        )
+
+    let usuarios = manager.read()
+    console.log("se llama a read -->",usuarios)
+
+
+    console.log('se crea un tercer usuario pero con datos incompletos-->')
+    manager.create(
+        "Usuario 3",
+        )
+
+
+    let getUserById =  manager.readOne(1)
+    console.log('se llama a readOne con el id 1-->', getUserById)
+
+    let getUserById2 =  manager.readOne(245605)
+    console.log('se llama a readOne con el id 205-->', getUserById2)
+}
+
+env()
